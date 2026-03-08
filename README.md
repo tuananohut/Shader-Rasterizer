f6d2382dd94a2fc53ead4976f6d1f9a9d95e5725
tuananohut <tkanesrkajol58@gmail.com>
2026-03-08 18:56:47 +0300

    new shaders

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
-# Wang Tile 
+# Shader Rasterizer
 
+This project is a small experimental CPU based shader rasterizer. 
+It generates procedural textures as PNG images using simple fragment shaders. 
+
+## Shader Examples
+
+### Stripes
+![Stripes](images/stripes.png)
+
+### Japan
+![Japan](images/japan.png)
+
+### Circles
+![Circles](images/circles.png)
+
+## Build & Run
+
+Run the build script to compile the program:
+
+```bash
+    ./build.sh
+```
+
+Run the program with a shader name as argument:
+
+```bash
+    ./build/program stripes
+```
+
+```bash
+    ./build/program circles
+```
+
+```bash
+    ./build/program japan
+```
+
+If no shader name is provided, the program will use stripes by default.
+The generated PNG file will be saved in the images/ folder as output.png
 
 ### References: 
 https://www.youtube.com/watch?v=_SufQh6OIzs&list=PLpM-Dvs8t0VYgJXZyQzWjfYUm3MxcvqR0&index=2
--- src/main.c
+++ src/main.c
@@ -4,14 +4,12 @@
 #include <string.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "../external/stb_image_write.h"
 
 #include "./mathlib.c"
 
 #define TILE_WIDTH 512
 #define TILE_HEIGHT 512
-#define TILESET_WIDTH (TILE_WIDTH * 4)
-#define TILESET_HEIGHT (TILE_HEIGHT * 4)
 
 typedef RGB (*Frag_Shader)(UV uv);
 typedef uint32_t RGBA32;
@@ -35,16 +33,50 @@ RGB stripes(UV uv)
 {
   float n = 20.f; 
   Vec3f v = 
-  {
+  { 
     sinf(uv.c[U] * n), 
     sinf((uv.c[U] + uv.c[V]) * n),
-    cosf(uv.c[V] * n)
+    cosf(uv.c[V] * n) 
   };
   
   return vec3f_mul(vec3f_sum(v, vec3fs(1.f)),
                vec3fs(0.5f));
 }
 
+RGB japan(UV uv) 
+{
+  float r = 0.25f; 
+  int a = vec2f_sqrlen(vec2f_sub(vec2fs(0.5f), uv)) > r * r;  
+  return vec3f(1.f, (float) a, (float) a);
+}
+
+RGB circles(UV uv) 
+{
+  float r1 = 0.25f; 
+  float r2 = 0.30f; 
+  float r3 = 0.35f; 
+  
+  float my_circle = vec2f_sqrlen(vec2f_sub(vec2fs(0.5f), uv));
+  
+  Vec3f color; 
+  
+  if (my_circle < r1*r1) 
+  {
+    color = vec3f(0.58f, 0.239f, 1.f); 
+  } 
+  else if (my_circle < r2*r2)
+  {
+    color = vec3f(0.596f, 0.165f, 0.416f);
+  }
+  else if (my_circle < r3*r3)
+  {
+    color = vec3f(0.f, 0.702f, 0.631f);
+  }
+  else 
+    color = vec3f(0.094f, 0.094f, 0.094f);
+  
+  return color; 
+}
 
 void generate_image32(Tile *tile, Frag_Shader shader) 
 {
@@ -60,7 +92,7 @@ void generate_image32(Tile *tile, Frag_Shader shader)
   }
 }
 
-int main() 
+int main(int argc, char *argv[]) 
 {
   uint32_t pixels[TILE_WIDTH * TILE_HEIGHT];
 
@@ -69,16 +101,49 @@ int main()
   tile.height = TILE_HEIGHT;
   tile.stride = TILE_WIDTH; 
   tile.pixels = pixels;
-
-  generate_image32(&tile, stripes); 
   
-  const char *output_file_path = "output.png";  
+  Frag_Shader shader = stripes; 
+  
+  char *shader_names[] = 
+  {
+    "stripes", 
+    "japan",
+    "circles"
+  };
+  size_t shader_name_count = sizeof(shader_names) / sizeof(shader_names[0]);
+  
+  if (argc < 2) 
+  {
+    printf("This is Wang Tile generator. Pick a shader:\n");
+    for (size_t i = 0; i < shader_name_count; ++i) 
+    {
+      printf("%s\n", shader_names[i]); 
+    }   
+    printf("Default stripes shader selected.\n");
+  } 
+  else 
+  {
+    char *shader_name = argv[1];
+  
+    if (strcmp(shader_name, "stripes") == 0)
+      shader = stripes; 
+    else if (strcmp(shader_name, "japan") == 0)
+      shader = japan; 
+    else if (strcmp(shader_name, "circles") == 0)
+      shader = circles;
+    else 
+      fprintf(stderr, "Unknown shader: %s\n", shader_name);
+  }
+  
+  generate_image32(&tile, shader); 
+  
+  const char *output_file_path = "../images/output.png";  
   if (!stbi_write_png(output_file_path, tile.width, tile.height, 4, pixels, tile.width * sizeof(RGBA32)))
   {
     fprintf(stderr, "ERROR: could not save file %s: %s\n", output_file_path,
             strerror(errno));
     exit(1);
   } 
-  
+
   return 0;
 }
